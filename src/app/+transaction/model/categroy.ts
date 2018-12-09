33efb0430385af403c6714b4ccbea18d37eb4045
André <tdc@gmx.ch>
2018-12-09 22:10:16 +0100

    add model

---
+++ src/app/+transaction/model/categroy.ts
@@ -0,0 +1,4 @@
+export class category {
+    $key: string;
+    name: string;
+}
---
+++ src/app/+transaction/model/transaction.ts
@@ -0,0 +1,7 @@
+
+export class Transaction {
+    $key: string;
+    description: string;
+    value: number;
+    category: string;
+}
