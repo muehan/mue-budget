56cd56be45abd096bce61782840f47c6beeec3ca
André <tdc@gmx.ch>
2018-12-09 13:26:43 +0100

    fix imports

--- src/app/+shared/shared.module.ts
+++ src/app/+shared/shared.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-  ]
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class SharedModule { }
