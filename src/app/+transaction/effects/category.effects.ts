5a8806bf89c2add0e21aa0f0eed04c7887f49015
Andre <andre.muehle@migrosluzern.ch>
2018-12-11 09:42:13 +0100

    try add effects

--- src/app/+transaction/effects/category.effects.ts
+++ src/app/+transaction/effects/category.effects.ts
@@ -6,17 +6,25 @@ import { CategoryActions } from '../actions';
 import { switchMap, mergeMap, map } from 'rxjs/operators';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Category } from '../model/categroy';
+import { Observable } from 'rxjs';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 @Injectable()
 export class CategoryEffectsService {
 
     constructor(
-        private actions$: Actions,
+        private actions: Actions,
         private db: AngularFireDatabase,
     ) { }
 
-    // @Effect()
-    // login$ = this.actions$
-    //     .ofType<CategoryActions.GetCategories>(CategoryActions.ActionTypes.GetCategories)
+    @Effect()
+    getPost: Observable<Action> = this.actions.ofType(CategoryActions.GetCategories)
+      .map((action: CategoryActions.GetCategories) => action )
+      .delay(2000) // delay to show spinner
+      .mergeMap(payload => this.db.object(payload))
+      .map(post => {
+        post.pushKey = post.$key;
+        return new CategoryActions.GetCategoriesSuccess(post);
+      });
        
 }
