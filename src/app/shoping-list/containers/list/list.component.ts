292903c13c92ad43cc05f5843fd6751abd60fdc6
André <tdc@gmx.ch>
2022-07-03 14:05:33 +0200

    catch null value in shopting list add dialog close

--- src/app/shoping-list/containers/list/list.component.ts
+++ src/app/shoping-list/containers/list/list.component.ts
@@ -1,23 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
-import { ShoppingItem } from '../../models/shopping-item';
-import { MatDialog } from '@angular/material/dialog';
-import { ListService } from '../../services/list.service';
-import { AddItemDialogComponent } from '../../dialogs/add-item/add-item.component';
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs/internal/Observable";
+import { ShoppingItem } from "../../models/shopping-item";
+import { MatDialog } from "@angular/material/dialog";
+import { ListService } from "../../services/list.service";
+import { AddItemDialogComponent } from "../../dialogs/add-item/add-item.component";
 
 @Component({
-  selector: 'mue-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  selector: "mue-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.scss"],
 })
 export class ListComponent implements OnInit {
-
   public items$: Observable<ShoppingItem[]>;
 
-  constructor(
-    public dialog: MatDialog,
-    private listService: ListService,
-  ) { }
+  constructor(public dialog: MatDialog, private listService: ListService) {}
 
   ngOnInit() {
     this.listService.initialize();
@@ -26,13 +22,15 @@ export class ListComponent implements OnInit {
 
   AddNewItem() {
     let dialogRef = this.dialog.open(AddItemDialogComponent);
-    dialogRef.afterClosed().subscribe(result => {
-      this.listService.add({ value: result, checked: false });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.listService.add({ value: result, checked: false });
+      }
     });
   }
 
   selectedItem(item) {
-    item.checked === true ? item.checked = false : item.checked = true;
+    item.checked === true ? (item.checked = false) : (item.checked = true);
     this.listService.update(item);
   }
 
